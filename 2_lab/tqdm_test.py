7fd0e98d6922389915d321dbc0ee1d2590bb50e1
Angelina2031Krasuliak <angelina.krasuliak@gmail.com>
2025-04-24 09:47:37 +0300

    remove 24.04.25

--- 2_lab/tqdm_test.py
+++ 2_lab/tqdm_test.py
@@ -1,5 +1,39 @@
 from tqdm import tqdm
 from time import sleep
-r = range(5)
-for i in tqdm(r):
-    sleep(1)
+import psutil
+import os
+import csv
+import string
+import random
+import psutil
+
+def get_memory_usage():
+    process = psutil.Process(os.getpid())
+    return process.memory_info().rss / 1024 / 1024  # в MB
+
+if __name__ == "__main__":
+    print(f"Початкова памʼять: {get_memory_usage():.2f} MB")
+
+    os.remove("./1.csv")
+    with open("1.csv",'a') as f:
+        wr = csv.writer (f)
+        for i in range(1000):
+            ss = "".join (random.choices(string.ascii_letters , k=3))
+            wr.writerow ([random.choice(string.digits), "tt", 0, ss])
+
+    print(f"Після читання файлу: {get_memory_usage():.2f} MB")
+
+    def get_csv():
+        with open("1.csv") as c:
+            rw = csv.reader(c)
+            for i, r in enumerate(rw):
+                #перше це номер рядка, друге власне значення.
+                yield i, r
+
+    for i, s in tqdm(get_csv(), total=1000):
+        #print (f"Читаємо стрічку{s}під номером {i}")
+        sleep(0.001)
+    #print (len(list(get_csv())))
+    print(f"Після читання файлу: {get_memory_usage():.2f} MB")
+
+    
