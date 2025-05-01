9000e74c70c290a43c11d80200568d10f0795335
Angelina2031Krasuliak <angelina.krasuliak@gmail.com>
2025-05-01 09:42:46 +0300

    working app 01.05.25

---
+++ 3_lab/web_app/src/web_app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+r = Redis(host='localhost', port=6379, db=0)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/info")
+def info():
+    C = r.incr("info_count")
+    return f"Інкрементуємо значення до {C}"
+
+@app.route("/del")
+def del_info():
+    C = r.decr("info_count")
+    return f"Декрементуємо значення до {C}"
+    
+
+if __name__ == "__main__":
+  
+    app.run(host="localhost", port=5000, debug=True)
